5b29e6541ee2c73ec1d4ebd21fc79143f2887b74
Carlos Alcala <carlos.alcala@me.com>
2022-02-12 23:26:55 -0400

    Base Restart App

---
+++ 1. RESTART/Restart-App/Restart/Pods/Target Support Files/Pods-Restart/Pods-Restart-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Restart : NSObject
+@end
+@implementation PodsDummy_Pods_Restart
+@end
---
+++ 1. RESTART/Restart-App/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Carlos Alcala on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
