f9984fb7ef51295f315f8892deec34d1aabe6048
Carlos Alcala <carlos.alcala@me.com>
2022-02-13 21:26:54 -0400

    Pinch Base Project with TapGesture

---
+++ 2. PINCH/Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Carlos Alcala on 13/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
