ca18c3c2c5c2d627d0293fef8fc694c42dbaab35
Carlos Alcala <carlos.alcala@me.com>
2022-02-20 12:13:08 -0400

    Final Touches

---
+++ 2. PINCH/Pinch/Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Carlos Alcala on 20/2/22.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
